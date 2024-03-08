6167abbe99e3e2327d976268b689739670d85ab5
ByteNode1 <89481803+ByteNode1@users.noreply.github.com>
2024-03-08 16:59:50 +0300

    Now stopid counter

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -5,19 +5,27 @@ import { MainContract } from "../wrappers/MainContract";
 import "@ton-community/test-utils";
 
 describe("main.fc contract tests", () => {
-  it("should get the proper most recent sender address", async () => {
+  it("Should successfully increase counter in contract and get the proper most recent sender address", async () => {
     const blockchain = await Blockchain.create();
+    
     const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
+    const initAddress = await blockchain.treasury("initAddress");
+
     const myContract = blockchain.openContract(
-      await MainContract.createFromConfig({}, codeCell)
+      await MainContract.createFromConfig(
+        {
+        number: 0,
+        address: initAddress.address
+      }, codeCell)
     );
 
     const senderWallet = await blockchain.treasury("sender");
 
-    const sentMessageResult = await myContract.sendInternalMessage(
+    const sentMessageResult = await myContract.sendIcrement(
       senderWallet.getSender(),
-      toNano("0.05")
+      toNano("0.05"),
+      1
     );
 
     expect(sentMessageResult.transactions).toHaveTransaction({
@@ -29,5 +37,6 @@ describe("main.fc contract tests", () => {
     const data = await myContract.getData();
 
     expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual(1);
   });
 });
